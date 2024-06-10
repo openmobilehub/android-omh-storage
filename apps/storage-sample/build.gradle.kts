e23888522d485c79134bd9bb6ef9be99a9ca958b
Maciej Budziński <maciej.budzinski.93@gmail.com>
2024-06-10 14:26:28 +0200

    refactor: reuse getValueFromEnvOrProperties util
    Signed-off-by: Maciej Budziński <maciej.budzinski.93@gmail.com>

--- apps/storage-sample/build.gradle.kts
+++ apps/storage-sample/build.gradle.kts
@@ -1,5 +1,3 @@
-import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
-
 plugins {
     `android-application`
     id("kotlin-kapt")
@@ -16,7 +14,7 @@ android {
         versionCode = 1
         versionName = "1.0"
 
-        val dropboxAppKey = getValueFromProperties("DROPBOX_APP_KEY")
+        val dropboxAppKey = getValueFromEnvOrProperties("DROPBOX_APP_KEY")
 
         resValue("string", "db_login_protocol_scheme", "db-${dropboxAppKey}")
     }
@@ -115,10 +113,4 @@ dependencies {
     }
 
     testImplementation(Libs.junit)
-}
-
-fun getValueFromProperties(name: String): String {
-    val properties = gradleLocalProperties(rootDir)
-    val property = properties[name] as? String
-    return property ?: throw GradleException("Missing property $name , please add it to the local.properties file")
 }
