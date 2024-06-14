f21255426f2e58a337308511938ab276790b3725
Maciej Budziński <maciej.budzinski.93@gmail.com>
2024-06-14 08:22:48 +0200

    feat: add dropbox upload
    Signed-off-by: Maciej Budziński <maciej.budzinski.93@gmail.com>

--- packages/plugin-dropbox/src/main/java/com/openmobilehub/android/storage/plugin/dropbox/data/repository/DropboxFileRepository.kt
+++ packages/plugin-dropbox/src/main/java/com/openmobilehub/android/storage/plugin/dropbox/data/repository/DropboxFileRepository.kt
@@ -3,6 +3,7 @@ package com.openmobilehub.android.storage.plugin.dropbox.data.repository
 import com.openmobilehub.android.storage.core.model.OmhFile
 import com.openmobilehub.android.storage.plugin.dropbox.data.mapper.MetadataToOmhFile
 import com.openmobilehub.android.storage.plugin.dropbox.data.service.DropboxApiService
+import java.io.File
 
 internal class DropboxFileRepository(
     private val apiService: DropboxApiService,
@@ -14,5 +15,15 @@ internal class DropboxFileRepository(
         return dropboxFiles.entries.mapNotNull {
             metadataToOmhFile(it)
         }
+    }
+
+    fun uploadFile(localFileToUpload: File, parentId: String): OmhFile? {
+        val inputStream = localFileToUpload.inputStream()
+
+        val path = "$parentId/${localFileToUpload.name}"
+
+        val response = apiService.uploadFile(inputStream, path)
+
+        return metadataToOmhFile(response)
     }
 }
--- packages/plugin-dropbox/src/main/java/com/openmobilehub/android/storage/plugin/dropbox/data/service/DropboxApiService.kt
+++ packages/plugin-dropbox/src/main/java/com/openmobilehub/android/storage/plugin/dropbox/data/service/DropboxApiService.kt
@@ -1,10 +1,16 @@
 package com.openmobilehub.android.storage.plugin.dropbox.data.service
 
+import com.dropbox.core.v2.files.FileMetadata
 import com.dropbox.core.v2.files.ListFolderResult
+import java.io.InputStream
 
 internal class DropboxApiService(private val apiClient: DropboxApiClient) {
 
     fun getFilesList(parentId: String): ListFolderResult {
         return apiClient.dropboxApiService.files().listFolder(parentId)
+    }
+
+    fun uploadFile(inputStream: InputStream, path: String): FileMetadata {
+        return apiClient.dropboxApiService.files().uploadBuilder(path).uploadAndFinish(inputStream)
     }
 }
