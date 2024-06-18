b39c142e6c3170459e2e6a9adcabd8e6e26ee7b2
Maciej Budziński <maciej.budzinski.93@gmail.com>
2024-06-18 10:01:41 +0200

    feat/OMHD-335: OneDrive file upload (#13)
    * feat: onedrive wip
    
    Signed-off-by: Maciej Budziński <maciej.budzinski.93@gmail.com>
    
    * chore: update gitignore
    
    Signed-off-by: Maciej Budziński <maciej.budzinski.93@gmail.com>
    
    * feat: onedrive file listings
    
    Signed-off-by: Maciej Budziński <maciej.budzinski.93@gmail.com>
    
    * ci: update pipelines
    
    Signed-off-by: Maciej Budziński <maciej.budzinski.93@gmail.com>
    
    * feat: update getting drive error handling
    
    Signed-off-by: Maciej Budziński <maciej.budzinski.93@gmail.com>
    
    * refactor: refactor onedrive implementation
    
    Signed-off-by: Maciej Budziński <maciej.budzinski.93@gmail.com>
    
    * test: add first tests
    
    Signed-off-by: Maciej Budziński <maciej.budzinski.93@gmail.com>
    
    * feat: adjust for release build
    
    Signed-off-by: Maciej Budziński <maciej.budzinski.93@gmail.com>
    
    * refactor: architectual improvements
    
    Signed-off-by: Maciej Budziński <maciej.budzinski.93@gmail.com>
    
    * chore: minSDK versions improvements
    
    Signed-off-by: Maciej Budziński <maciej.budzinski.93@gmail.com>
    
    * test: add more unit tests to onedrive plugin
    
    Signed-off-by: Maciej Budziński <maciej.budzinski.93@gmail.com>
    
    * chore: update jacocoCoverageVerification dependsOn map
    
    Signed-off-by: Maciej Budziński <maciej.budzinski.93@gmail.com>
    
    * chore: restore ms_auth_config.json
    
    Signed-off-by: Maciej Budziński <maciej.budzinski.93@gmail.com>
    
    * refactor: changes after self CR
    
    Signed-off-by: Maciej Budziński <maciej.budzinski.93@gmail.com>
    
    * feat: add onedrive file upload
    
    Signed-off-by: Maciej Budziński <maciej.budzinski.93@gmail.com>
    
    * fix: fix uploadFile method
    
    Signed-off-by: Maciej Budziński <maciej.budzinski.93@gmail.com>
    
    * test: add tests
    
    Signed-off-by: Maciej Budziński <maciej.budzinski.93@gmail.com>
    
    * chore: remove ms_auth_config from git
    
    Signed-off-by: Maciej Budziński <maciej.budzinski.93@gmail.com>
    
    * chore: add empty ms_auth_config
    
    Signed-off-by: Maciej Budziński <maciej.budzinski.93@gmail.com>
    
    ---------
    
    Signed-off-by: Maciej Budziński <maciej.budzinski.93@gmail.com>

--- packages/plugin-onedrive/src/main/java/com/openmobilehub/android/storage/plugin/onedrive/data/repository/OneDriveFileRepository.kt
+++ packages/plugin-onedrive/src/main/java/com/openmobilehub/android/storage/plugin/onedrive/data/repository/OneDriveFileRepository.kt
@@ -3,6 +3,7 @@ package com.openmobilehub.android.storage.plugin.onedrive.data.repository
 import com.openmobilehub.android.storage.core.model.OmhFile
 import com.openmobilehub.android.storage.plugin.onedrive.data.mapper.DriveItemToOmhFile
 import com.openmobilehub.android.storage.plugin.onedrive.data.service.OneDriveApiService
+import java.io.File
 
 class OneDriveFileRepository(
     private val apiService: OneDriveApiService,
@@ -12,5 +13,11 @@ class OneDriveFileRepository(
         return apiService.getFilesList(parentId).map {
             driveItemToOmhFile(it)
         }
+    }
+
+    fun uploadFile(localFileToUpload: File, parentId: String): OmhFile? {
+        val driveItem = apiService.uploadFile(localFileToUpload, parentId)
+
+        return driveItem?.let { driveItemToOmhFile(it) }
     }
 }
