7fe781223495ffbf66fa675d6dae82612d614056
Maciej Budziński <maciej.budzinski.93@gmail.com>
2024-06-20 14:54:05 +0200

    ci: update setup_secrets script
    Signed-off-by: Maciej Budziński <maciej.budzinski.93@gmail.com>

--- .github/scripts/setup_secrets.sh
+++ .github/scripts/setup_secrets.sh
@@ -3,3 +3,6 @@ echo "GOOGLE_CLIENT_ID=$1" >> ./local.properties
 echo "DROPBOX_APP_KEY=$2" >> ./local.properties
 echo "MICROSOFT_CLIENT_ID=$3" >> ./local.properties
 echo "MICROSOFT_SIGNATURE_HASH=$4" >> ./local.properties
+
+# Creating a new file form ms_auth_config.json
+echo "{}" > ./apps/storage-sample/src/main/res/raw/ms_auth_config.json
