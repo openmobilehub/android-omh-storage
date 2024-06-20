9fea7c3d531911a154036cc9878c310b17c203e1
Maciej Budziński <maciej.budzinski.93@gmail.com>
2024-06-20 14:57:42 +0200

    ci: update setup_secrets script
    Signed-off-by: Maciej Budziński <maciej.budzinski.93@gmail.com>

--- .github/scripts/setup_secrets.sh
+++ .github/scripts/setup_secrets.sh
@@ -5,4 +5,5 @@ echo "MICROSOFT_CLIENT_ID=$3" >> ./local.properties
 echo "MICROSOFT_SIGNATURE_HASH=$4" >> ./local.properties
 
 # Creating a new file form ms_auth_config.json
+ls -al
 echo "{}" > ./apps/storage-sample/src/main/res/raw/ms_auth_config.json
