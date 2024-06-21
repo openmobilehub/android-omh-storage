43db9c09b94063ced61d70318dae0ee1322e945e
DamianNowak5f <damian.nowak.5f@gmail.com>
2024-06-21 11:26:22 +0200

    doc: add missing copyright comments
    Signed-off-by: DamianNowak5f <damian.nowak.5f@gmail.com>

--- apps/storage-sample/src/main/java/com/openmobilehub/android/storage/sample/domain/model/FileTypeMapper.kt
+++ apps/storage-sample/src/main/java/com/openmobilehub/android/storage/sample/domain/model/FileTypeMapper.kt
@@ -1,5 +1,3 @@
-package com.openmobilehub.android.storage.sample.domain.model
-
 /*
  * Copyright 2023 Open Mobile Hub
  *
@@ -15,6 +13,8 @@ package com.openmobilehub.android.storage.sample.domain.model
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package com.openmobilehub.android.storage.sample.domain.model
 
 object FileTypeMapper {
 
